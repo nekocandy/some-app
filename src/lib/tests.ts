29bc96ee80543eedd8fd5fb7660e3c8d4b3267c9
Piyush <bhangalepiyush@gmail.com>
2023-05-14 01:55:26 +0530

    feat: add images to availableTests

--- src/lib/tests.ts
+++ src/lib/tests.ts
@@ -1,31 +1,36 @@
 export const availableTests = [
   {
     name: "TB",
-    image: "",
+    image:
+      "https://media.istockphoto.com/id/949160140/photo/secondary-tuberculosis-in-lungs-and-close-up-view-of-mycobacterium-tuberculosis-bacteria.jpg?s=612x612&w=0&k=20&c=npxb9gFZ_80NqiDEMv372Ln9fhXbOKYmJsHC9fljJHU=",
     id: "1II6xjqNr",
     url: "https://teachablemachine.withgoogle.com/models/1II6xjqNr/",
   },
   {
     name: "Pneumonia",
-    image: "",
+    image:
+      "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202206/MIT-Pneumonia-Sensor-01-press_0.jpg?itok=pY607Gyu",
     id: "qb_vOHcMK",
     url: "https://teachablemachine.withgoogle.com/models/qb_vOHcMK/",
   },
   {
-    name: "BrainTumor",
-    image: "",
+    name: "Brain Tumor",
+    image:
+      "https://www.matherhospital.org/wp-content/uploads/2017/09/brain-tumor-blog-photo.jpg",
     id: "C6rLiZv7C",
     url: "https://teachablemachine.withgoogle.com/models/C6rLiZv7C/",
   },
   {
     name: "Osteoporosis Knee",
-    image: "",
+    image:
+      "https://news.northwestern.edu/assets/Stories/2022/05/knee1940__FitMaxWzk3MCw2NTBd.jpg",
     id: "bFsiRQSQW",
     url: "https://teachablemachine.withgoogle.com/models/bFsiRQSQW/",
   },
   {
     name: "Caries",
-    image: "",
+    image:
+      "https://www.dentistrytoday.com/wp-content/uploads/2022/05/Figure-6-1000x650.png",
     id: "XUGeVRxO5",
     url: "https://teachablemachine.withgoogle.com/models/XUGeVRxO5/",
   },
