b849590d2827e1eaaa31a3997392b1d5e573a78d
Piyush <bhangalepiyush@gmail.com>
2023-05-14 01:56:02 +0530

    feat: add health-status page to display available tests

--- src/pages/health-status/index.tsx
+++ src/pages/health-status/index.tsx
@@ -1,11 +1,26 @@
 import Disclaimer from "~/components/HealthStatus/Disclaimer";
+import TestCard from "~/components/HealthStatus/TestCard";
 import HomepageSidebar from "~/components/HompageSidebar";
+import { availableTests } from "~/lib/tests";
 
 export default function HealthStatusCards() {
   return (
     <div className="flex h-full w-full flex-1">
       <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-8"></div>
+        {/*  first columns for cards */}
+        <div className="col-span-8 rounded-xl bg-[#1A5F7A]">
+          <div className="flex h-full flex-col items-center justify-center gap-8 overflow-y-auto px-6 py-8 text-white">
+            <div className="text-center text-2xl font-bold tracking-wide">
+              Available Tests
+            </div>
+
+            <div className="flex h-full w-full flex-wrap justify-center gap-4">
+              {availableTests.map((test) => (
+                <TestCard key={test.id} {...test} />
+              ))}
+            </div>
+          </div>
+        </div>
 
         <div className="col-span-4 rounded-xl bg-[#088395]">
           <HomepageSidebar
