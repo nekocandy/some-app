1173b5aa21945d5464b63fc9d79a811d5e69277a
Piyush <bhangalepiyush@gmail.com>
2023-05-13 12:00:37 +0530

    feat: realm

--- src/lib/mongo/init.ts
+++ src/lib/mongo/init.ts
@@ -1,10 +1,10 @@
 import * as Realm from "realm-web";
 import { env } from "~/env.mjs";
 
-const globalForPrisma = globalThis as unknown as {
+const globalForRealm = globalThis as unknown as {
   realm: Realm.App | undefined;
 };
 
 export const realm =
-  globalForPrisma.realm ?? new Realm.App({ id: env.NEXT_PUBLIC_REALM_APP_ID });
-if (env.NODE_ENV !== "production") globalForPrisma.realm = realm;
+  globalForRealm.realm ?? new Realm.App({ id: env.NEXT_PUBLIC_REALM_APP_ID });
+if (env.NODE_ENV !== "production") globalForRealm.realm = realm;
