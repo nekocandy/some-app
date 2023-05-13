441537fc3f38a92db4b8c2fa2c4f655cc1cf8495
Piyush <bhangalepiyush@gmail.com>
2023-05-13 23:08:24 +0530

    feat: name card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -34,7 +34,7 @@ export default function DashboardPage() {
 
   return (
     <div className="flex h-full w-full items-center justify-center gap-4">
-      <div className="grid h-full w-full grid-cols-12">
+      <div className="grid h-full w-full grid-cols-12 gap-6">
         <div className="col-span-8 rounded-xl bg-[#1A5F7A]">
           <div className="flex flex-col gap-6 px-8 py-4">
             {/* title */}
@@ -69,17 +69,48 @@ export default function DashboardPage() {
 
             <div className="space-y-4">
               <span className="text-xl text-white">Upcoming appointments</span>
-              <div className="rounded-xl bg-[#088395] px-4 py-4 flex flex-col gap-4">
+              <div className="flex flex-col gap-4 rounded-xl bg-[#088395] px-4 py-4">
                 {appointments.map((appointment, index) => (
                   <div
                     key={index}
-                    className="flex w-full items-center justify-between text-[#C1F5A7] text-lg"
+                    className="flex w-full items-center justify-between text-lg text-[#C1F5A7]"
                   >
                     {appointment.doctor}
                     <IconArrowBadgeRight color="#C1F5A7" />
                     {appointment.date.toDateString()}
                   </div>
                 ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* right bar */}
+        <div className="col-span-4 rounded-xl bg-[#088395]">
+          <div className="flex h-full w-full flex-col items-center gap-6 px-6 pt-32">
+            <div className="w-full flex flex-col bg-[#134a52] rounded-xl">
+              <div className="flex w-full items-center justify-center gap-4 rounded-lg bg-[#57c5b6] py-8 text-black">
+                <div>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src="/pfp.png"
+                    className="h-20 w-20 rounded-full border-4 border-[#002B5B]"
+                    alt="pfp"
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-2xl font-medium">
+                    Ei, Raiden Shogun
+                  </span>
+                  <span className="text-xs">
+                    <span className="font-medium">Age:</span> 20
+                    <span>F</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="text-center px-4 py-2 text-[#C1F5A7]">
+              Not under any Diagnosis 
               </div>
             </div>
           </div>
