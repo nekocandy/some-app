8ecf6be1b8a2626e93d89ebac6ff685ce2d4ef3b
Piyush <bhangalepiyush@gmail.com>
2023-05-14 01:47:57 +0530

    feat: add available tests

---
+++ src/lib/tests.ts
@@ -0,0 +1,32 @@
+export const availableTests = [
+  {
+    name: "TB",
+    image: "",
+    id: "1II6xjqNr",
+    url: "https://teachablemachine.withgoogle.com/models/1II6xjqNr/",
+  },
+  {
+    name: "Pneumonia",
+    image: "",
+    id: "qb_vOHcMK",
+    url: "https://teachablemachine.withgoogle.com/models/qb_vOHcMK/",
+  },
+  {
+    name: "BrainTumor",
+    image: "",
+    id: "C6rLiZv7C",
+    url: "https://teachablemachine.withgoogle.com/models/C6rLiZv7C/",
+  },
+  {
+    name: "Osteoporosis Knee",
+    image: "",
+    id: "bFsiRQSQW",
+    url: "https://teachablemachine.withgoogle.com/models/bFsiRQSQW/",
+  },
+  {
+    name: "Caries",
+    image: "",
+    id: "XUGeVRxO5",
+    url: "https://teachablemachine.withgoogle.com/models/XUGeVRxO5/",
+  },
+];
