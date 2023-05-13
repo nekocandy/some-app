df9992ed0106bdb7c9377d4446a486b2049e66e2
Piyush <bhangalepiyush@gmail.com>
2023-05-13 15:26:00 +0530

    feat: layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,17 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="h-screen bg-[#002B5B]">
+      <main className="flex h-screen gap-2 py-4">
+        <div className="flex h-full justify-center overflow-y-auto px-4"></div>
+
+        <div className="h-full w-full flex-1 overflow-y-auto py-8 pr-8">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
