1b8a8c24a103e5588f66e8e19904a6099097607b
Piyush <bhangalepiyush@gmail.com>
2023-05-14 01:58:45 +0530

    feat: navigate to dashboard if user is signed in

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -64,7 +64,7 @@ export default function Sidebar() {
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-between py-8 pr-8">
-      <Link href="/">
+      <Link href={realm.currentUser ? "/dashboard" : "/"}>
         <div className="flex items-center gap-2">
           <div className="h-10 w-10 rounded-full bg-[#0A4D68]" />
           <span className="text-xl font-bold tracking-wide text-[#0A4D68]">
