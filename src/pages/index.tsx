42ee0468c6ccf44ddd8b7e0f9bddfe5e025d9e90
Piyush <bhangalepiyush@gmail.com>
2023-05-13 15:25:52 +0530

    feat: sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,9 @@ import PredictionImage from "~/components/PredictionImage";
 import { createUser } from "~/lib/mongo/auth";
 
 const Home: NextPage = () => {
-
-  return (
-    <div>
-      <PredictionImage
-        modelURL="https://teachablemachine.withgoogle.com/models/XUGeVRxO5/"
-      />
-
-      <button onClick={() => {
-        void createUser("bhangalepiyush@gmail.com", "test123&we4r");
-      }}>
-        Test
-      </button>
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 
 export default Home;
