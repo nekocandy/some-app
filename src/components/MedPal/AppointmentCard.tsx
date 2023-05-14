6821f66cc1aa6c2df0d3ed708d7c1e42cf091625
Piyush <bhangalepiyush@gmail.com>
2023-05-14 15:52:20 +0530

    feat: better appointment card

--- src/components/MedPal/AppointmentCard.tsx
+++ src/components/MedPal/AppointmentCard.tsx
@@ -19,11 +19,7 @@ export default function AppointmentCard({
 }: ChatBubbleProps) {
   return (
     <div className="flex w-full justify-start">
-      <div className="flex w-3/4 flex-col items-center justify-center gap-6 rounded-r-2xl rounded-t-2xl bg-[#1C4676] px-8 py-4">
-        <div className="rounded-full bg-white/40 px-4 py-1 text-xs text-black">
-          {message}
-        </div>
-
+      <div className="flex w-3/4 flex-col items-center justify-center gap-4 rounded-r-2xl rounded-t-2xl bg-[#1C4676] px-8 py-4">
         <div className="flex items-center gap-2">
           <div className="rounded-full bg-green-400/40 p-1">
             <IconCheck />
@@ -33,14 +29,18 @@ export default function AppointmentCard({
 
         <div className="flex flex-col items-center justify-center gap-1">
           <div className="flex items-center gap-2 text-xs">
-            <IconClock />
+            <IconClock className="h-5 w-5" />
             {format(new Date(date), "dd-MMM-yyyy hh:mm a")}
           </div>
 
           <div className="flex items-center gap-2 text-xs">
-            <IconLocation />
+            <IconLocation className="h-5 w-5" />
             {location}
           </div>
+        </div>
+
+        <div className="rounded-full bg-white/40 px-4 py-1 text-xs text-black">
+          {message}
         </div>
       </div>
     </div>
